13d290b76e5bff4c81a54a6bed7658b4aad34305
sr-sagar <sagarramani992@gmail.com>
2025-09-28 22:12:40 +0530

    made changes

--- r_motors/lib/prisma.ts
+++ r_motors/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../node_modules/@prisma/client"
+import { PrismaClient } from "@prisma/client"
 
 const globalPrismaClient = global as unknown as {prisma: PrismaClient}
 
