4aeba4e37168dbf93382015ec9b11005057c9924
sr-sagar <sagarramani992@gmail.com>
2025-09-29 00:04:06 +0530

    allowed origin with a middleware.ts file at root

---
+++ r_motors/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://rmotors-theta.vercel.app"
+]
+export const middleware = async(req: NextRequest) => {
+    const origin = req.headers.get("origin");
+    const res = NextResponse.next();
+
+    if(origin && allowedOrigins.includes(origin))
+    {
+        res.headers.set("Access-Control-Allow-Origin", origin);
+
+    }
+
+    res.headers.set("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
+    res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+
+    if(req.method === "OPTIONS")
+    {
+        return new NextResponse(null, {status: 204,headers: res.headers});
+    }
+
+    return res;
+}
+
+export const config = {
+    matcher: "/:path*",
+}
