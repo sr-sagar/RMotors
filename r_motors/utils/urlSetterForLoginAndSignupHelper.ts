3ffad2b1b9706bc86b2925cca8d5e6c4b902308a
sr-sagar <sagarramani992@gmail.com>
2025-09-29 11:08:50 +0530

    made changes to the helper

--- r_motors/utils/urlSetterForLoginAndSignupHelper.ts
+++ r_motors/utils/urlSetterForLoginAndSignupHelper.ts
@@ -2,7 +2,7 @@
 export const getBackendUrl = () => {
     if(process.env.NODE_ENV === "development")
     {
-        return "http:localhost:3000";
+        return "http://localhost:3000";
     }
     
     return "https://rmotors-theta.vercel.app";
