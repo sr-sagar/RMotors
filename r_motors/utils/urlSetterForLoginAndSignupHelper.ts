5c901ce23664bb6d8586f84f3a05f727ed41ab33
sr-sagar <sagarramani992@gmail.com>
2025-09-29 11:08:07 +0530

    made changes to the helper

--- r_motors/utils/urlSetterForLoginAndSignupHelper.ts
+++ r_motors/utils/urlSetterForLoginAndSignupHelper.ts
@@ -1,6 +1,5 @@
-"use server"
 
-export const getBackendUrl = async() => {
+export const getBackendUrl = () => {
     if(process.env.NODE_ENV === "development")
     {
         return "http:localhost:3000";
