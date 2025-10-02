727ca91be5007d40e7658f207f64a2b817fda0d5
sr-sagar <sagarramani992@gmail.com>
2025-10-02 18:12:58 +0530

    changed it so the notification propt only once per session

--- r_motors/components/features/chats/chatComponent.tsx
+++ r_motors/components/features/chats/chatComponent.tsx
@@ -45,9 +45,7 @@ const ChatComponent = ({chatTitle,unreadCount,productImageURL,roomId,messageDeta
 
   useEffect(() => {
     (async() => {
-
-      if(typeof window === "undefined") return;
-      if(sessionStorage.getItem("isNotificationAsked") !== "true") return
+      if(sessionStorage.getItem("isNotificationAsked") === "true") return
       if(Notification.permission !== "granted")
       {
 
@@ -63,7 +61,7 @@ const ChatComponent = ({chatTitle,unreadCount,productImageURL,roomId,messageDeta
             Notification.requestPermission()
           }
           sessionStorage.setItem("isNotificationAsked", "true")
-        }
+      }
       
     })();
   },[])
